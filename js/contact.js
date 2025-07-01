9538bd65fe76b9d390095aff4784041dbccff876
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-06-30 23:10:51 -0500

    Upload from Lambda

---
+++ js/contact.js
@@ -0,0 +1,20 @@
+{
+  "contact_images": [
+    {
+      "description": "Modern office building exterior with glass facade",
+      "filename": "contact1.jpg"
+    },
+    {
+      "description": "Smartphone with call screen visible",
+      "filename": "contact2.jpg"
+    },
+    {
+      "description": "Email icon on computer screen",
+      "filename": "contact3.jpg"
+    },
+    {
+      "description": "Wall clock showing business hours",
+      "filename": "contact4.jpg"
+    }
+  ]
+}
