f1984ee13abb0a655cd288e78e015d9e86156808
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-06-30 23:10:46 -0500

    Upload from Lambda

---
+++ js/index.js
@@ -0,0 +1,17 @@
+[
+    {
+        "name": "cloud server",
+        "path": "images/index1.jpg",
+        "alt": "asdfsa cloud platform interface"
+    },
+    {
+        "name": "business dashboard",
+        "path": "images/index2.jpg",
+        "alt": "asdfsa control panel interface"
+    },
+    {
+        "name": "secure hosting",
+        "path": "images/icon-security.svg",
+        "alt": "Security icon"
+    }
+]
